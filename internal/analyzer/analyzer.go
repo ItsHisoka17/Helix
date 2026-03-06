8c2aadc2f825f8f2ee039aff08e5579083cd2540
Jxdn <hashertechnical15@gmail.com>
2026-03-06 17:26:32 -0500

    Analyzer | Types/AnaylysisResult Update

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -1,5 +1,21 @@
 package analyzer
 
-func analyze() {
+func Analyze(projectpath string) (*AnalysisResult, error) {
+	deps, err := ParsePackageJson(projectpath)
+	if err != nil {
+		return nil, err
+	}
+	framework, dbs, redis := DetectDependencies(deps)
+	signals, ports, err := ParseCodeContext(projectpath)
+	if err != nil {
+		return nil, err
+	}
 
+	return &AnalysisResult{
+		Framework:  framework,
+		Databases:  dbs,
+		Redis:      redis,
+		RawSignals: signals,
+		Ports:      ports,
+	}, nil
 }
